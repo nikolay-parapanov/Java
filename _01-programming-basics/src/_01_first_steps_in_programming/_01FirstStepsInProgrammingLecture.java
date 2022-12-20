bb8f65c838115cc8d16ba7fa9f5021d7df5dbf8b
Nikolay Parapanov <n.parapanoff@gmail.com>
2022-12-20 23:31:37 +0200

    test

---
+++ _01-programming-basics/src/_01_first_steps_in_programming/_01FirstStepsInProgrammingLecture.java
@@ -0,0 +1,7 @@
+package _01_first_steps_in_programming;
+
+public class _01FirstStepsInProgrammingLecture {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
