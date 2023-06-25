c482d87dc12015a0ed58f144707c99fe751119b0
nikolay-parapanov <n.parapanoff@gmail.com>
2023-06-25 13:02:59 +0300

    some JUG academy problems

---
+++ JUG_Academy/OOP/FindMax.java
@@ -0,0 +1,27 @@
+public class FindMax {
+
+    public static void main(String[] args) {
+
+    }
+    public static void main(String[] args) {
+
+        System.out.println(findMax(intArray));
+        System.out.println(findMax(intArray));
+        System.out.println(findMax(intArray));
+
+    }
+
+    public static <T extends Comparable<T>> T findMax(T... elements) {
+        if (elements.length == 0) {
+            throw new IllegalArgumentException("Array cannot be empty.");
+        }
+
+        T max = elements[0];
+        for (int i = 1; i < elements.length; i++) {
+            if (elements[i].compareTo(max) > 0) {
+                max = elements[i];
+            }
+        }
+        return max;
+    }
+}
